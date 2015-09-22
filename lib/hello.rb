7b20d6402ae168aacb966901e01dc38f93dae671
Martin Niño <m_nino5@hotmail.com>
2015-09-21 21:53:11 -0500

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
-# Default is "World"
-# Author: Jim Weirich (jim@somewhere.com)
-name = ARGV.first || "World"
-puts "Hello, #{name}!"
+require 'greeter'
 
+# Default is World
+name = ARGV.first || "World"
+
+greeter = Greeter.new(name)
+puts greeter.greet
